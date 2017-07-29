76333b5ebbdc154e1f104ae160f9fe683ccfdc93
svetun <sb2g14@soton.ac.uk>
2017-07-29 22:04:20 +0100

    Remember me button remobed from login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,15 +41,15 @@
                                 </div>
                             </div>
                             {{--remember me button, use remember_token--}}
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
+                            {{--<div class="form-group">--}}
+                                {{--<div class="col-md-6 col-md-offset-4">--}}
+                                    {{--<div class="checkbox">--}}
+                                        {{--<label>--}}
+                                            {{--<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me--}}
+                                        {{--</label>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
 
                             <div class="form-group">
                                 <div class="col-md-8 col-md-offset-4">
