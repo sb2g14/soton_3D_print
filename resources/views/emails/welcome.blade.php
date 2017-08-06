9ae86e22cd5ab821757a25b37fdb3333de9b8efd
whimsial <ai1v14@soton.ac.uk>
2017-08-06 14:14:10 +0100

    Production version 1.2
    Merged with the changes + updated registration email. Users can be deleted from the website now.

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,16 +2,17 @@
 
 # Welcome!
 
-Thank you for registering!
+This is a confirmation of successful registration. You are now able to use the workshop site. Please check the
+Announcements section for the latest announcements and news about this application and the workshop.
 
 @component('mail::button', ['url' => 'http://3dprint.clients.soton.ac.uk'])
-Continue browsing
+Return to the workshop
 @endcomponent
 
 @component('mail::panel', ['url' => ''])
 3D printing is awesome!
 @endcomponent
 
-Thanks,<br>
+Kind Regards,<br>
 {{ config('app.name') }}
 @endcomponent
