c7ce52e4f81ad839fdf5f4d36ba5ad5135313b07
whimsial <ai1v14@soton.ac.uk>
2017-10-02 21:44:21 +0100

    testing 2
    testing table with restart

--- resources/views/printingData/finished.blade.php
+++ resources/views/printingData/finished.blade.php
@@ -35,42 +35,42 @@
                     <th>Restart</th>
                 </tr>
             </thead>
-            {{--<tbody>--}}
-                {{--@foreach($finished_jobs as $job)--}}
-                    {{--Separate hours from minutes and seconds in printing time--}}
-                    {{--@php( list($h, $i, $s) = explode(':', $job->time) )--}}
-                    {{--Add number of hours job takes to the time when it was approved--}}
-                    {{--Add number of minutes job takes--}}
-                    {{--Plus 15 minutes--}}
-                    {{--If time for job finish plus 15 minutes didn't pass we show approved job--}}
-                    {{--@if ($job->updated_at->addHour($h)->addMinutes($i)->addMinutes(15)->gte(Carbon\Carbon::now('Europe/London')))--}}
-                    {{--<tr class="text-left">--}}
-                        {{--<td data-th="ID">{{ $job->id }}</td>--}}
-                        {{--<td data-th="Printer No">{{ $job->printers_id }}</td>--}}
-                        {{--<td data-th="Name">{{$job->student_name}}</td>--}}
-                        {{--<td data-th="Payment Category">{{$job->payment_category}}</td>--}}
-                        {{--<td data-th="Time">{{ date("H:i", strtotime($job->time)) }}</td>--}}
-                        {{--<td data-th="Material Amount">{{ $job->material_amount }} g</td>--}}
-                        {{--<td data-th="Price">£{{ $job->price }}</td>--}}
-                        {{--<td data-th="Created on">{{ $job->created_at->toDateTimeString() }}</td>--}}
-                        {{--<td data-th="Approved on">{{ $job->updated_at->toDateTimeString() }}</td>--}}
-                        {{--<td data-th="Approved by">{{ $job->user->name }}</td>--}}
-                        {{--<td data-th="Use Case">{{ $job->use_case  }}</td>--}}
-                        {{--<td data-th="Successful">{{ $job->successful }}</td>--}}
-                        {{--<td data-th="Edit">--}}
-                        {{--@hasanyrole('LeadDemonstrator|administrator|3dhubs_manager')--}}
-                        {{--<a href="/printingData/edit/{{$job->id}}" class="btn btn-danger">Review Job</a>--}}
-                        {{--@endhasanyrole--}}
-                        {{--</td>--}}
-                        {{--<td data-th="Restart">--}}
+            <tbody>
+                @foreach($finished_jobs as $job)
+                    Separate hours from minutes and seconds in printing time
+                    @php( list($h, $i, $s) = explode(':', $job->time) )
+                    Add number of hours job takes to the time when it was approved
+                    Add number of minutes job takes
+                    Plus 15 minutes
+                    If time for job finish plus 15 minutes didn't pass we show approved job
+                    @if ($job->updated_at->addHour($h)->addMinutes($i)->addMinutes(15)->gte(Carbon\Carbon::now('Europe/London')))
+                    <tr class="text-left">
+                        <td data-th="ID">{{ $job->id }}</td>
+                        <td data-th="Printer No">{{ $job->printers_id }}</td>
+                        <td data-th="Name">{{$job->student_name}}</td>
+                        <td data-th="Payment Category">{{$job->payment_category}}</td>
+                        <td data-th="Time">{{ date("H:i", strtotime($job->time)) }}</td>
+                        <td data-th="Material Amount">{{ $job->material_amount }} g</td>
+                        <td data-th="Price">£{{ $job->price }}</td>
+                        <td data-th="Created on">{{ $job->created_at->toDateTimeString() }}</td>
+                        <td data-th="Approved on">{{ $job->updated_at->toDateTimeString() }}</td>
+                        <td data-th="Approved by">{{ $job->user->name }}</td>
+                        <td data-th="Use Case">{{ $job->use_case  }}</td>
+                        <td data-th="Successful">{{ $job->successful }}</td>
+                        <td data-th="Edit">
+                        @hasanyrole('LeadDemonstrator|administrator|3dhubs_manager')
+                        <a href="/printingData/edit/{{$job->id}}" class="btn btn-danger">Review Job</a>
+                        @endhasanyrole
+                        </td>
+                        <td data-th="Restart">
                         {{--@if($job->successful == 'No')--}}
-                        {{--<a href="/printingData/restart/{{$job->id}}" class="btn btn-primary">Restart</a>--}}
+                        <a href="/printingData/restart/{{$job->id}}" class="btn btn-primary">Restart</a>
                         {{--@endif--}}
-                        {{--</td>--}}
-                    {{--</tr>--}}
-                    {{--@endif--}}
-                {{--@endforeach--}}
-            {{--</tbody>--}}
+                        </td>
+                    </tr>
+                    @endif
+                @endforeach
+            </tbody>
         </table>
     </div>
 
