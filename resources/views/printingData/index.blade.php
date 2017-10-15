d8f16d52a49ee14f7b657a58a3415bbc30b09981
Ilya Khromov <ilyavkhromov@gmail.com>
2017-10-15 14:14:27 +0300

    front end changes
    Added navtab to printing data index.blade

--- resources/views/printingData/index.blade.php
+++ resources/views/printingData/index.blade.php
@@ -8,8 +8,28 @@
     @endif
 
    
-    <div class="text-center m-b-md">
+    <div class=" container text-center m-b-md">
         <div class="title">Pending Jobs</div>
+        <ul class="nav nav-tabs nav-justified">
+            <li class="active"><a data-toggle="tab" href="#home">Pending Jobs</a></li>
+            <li><a data-toggle="tab" href="#menu1">Approved Jobs / Printing</a></li>
+            <li><a data-toggle="tab" href="#menu2">Completed Jobs</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active">
+              <h3>HOME</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div id="menu1" class="tab-pane fade">
+              <h3>Menu 1</h3>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div id="menu2" class="tab-pane fade">
+              <h3>Menu 2</h3>
+              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            </div>
+        </div>
         <a href="/printingData/approved" type="button" class="btn btn-lg btn-success" style="display: inline-block;">Show currently approved jobs</a>
 
         @hasanyrole('LeadDemonstrator|administrator')
