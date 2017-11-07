996945919ddd1306ca0af7ef3d3e934620f14f0e
Ilya Khromov <ilyavkhromov@gmail.com>
2017-11-08 00:16:52 +0200

    Fixed mobile menu bug

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -277,6 +277,15 @@
                     </div>
                 </div>
 
+                
+
+
+
+
+
+
+
+
                 @else
                 <div class="row is-table-row">
                     <div class="col-sm-4 item">
@@ -372,6 +381,28 @@
                 </div>
             @endif
         </div>
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Modal Header</h4>
+              </div>
+              <div class="modal-body">
+                <p>Some text in the modal.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        
     </section>
 @endsection
 @section('scripts')
