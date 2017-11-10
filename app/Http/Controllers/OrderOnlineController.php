ee8a9cfe4a82b87b062aba60946e3c193f8ac243
whimsial <ai1v14@soton.ac.uk>
2017-11-10 15:44:17 +0000

    Scaffold for the online request store method
    Scaffold for the online request store method

--- app/Http/Controllers/OrderOnlineController.php
+++ app/Http/Controllers/OrderOnlineController.php
@@ -8,6 +8,11 @@ use App\Rules\SotonID;
 use App\Rules\SotonIdMinMax;
 use Illuminate\Http\Request;
 use App\Rules\CustomerNameValidation;
+use App\JobsPrints;
+use App\printers;
+use App\Jobs;
+use App\Prints;
+use App\cost_code;
 
 class OrderOnlineController extends Controller
 {
@@ -23,7 +28,8 @@ class OrderOnlineController extends Controller
 
     public function store()
     {
-        request()->validate([
+        // Validate the online request
+        $online_request = request()->validate([
             'student_name' => [
                 'required',
                 'string',
@@ -50,5 +56,29 @@ class OrderOnlineController extends Controller
                 new Alphanumeric
             ]
         ]);
+        // Store the online request in the database
+        $job = new Jobs;
+
+        $job -> total_material_amount = NULL;
+        $job -> total_price = NULL;
+        $job -> total_duration = NULL;
+        $job -> paid = 'No';
+        $job -> payment_category = NULL;
+        $job -> cost_code = NULL;
+        $job -> use_case = $online_request('use_case');
+        $job -> customer_id = $online_request('student_id');
+        $job -> customer_name = $online_request('student_name');
+        $job -> customer_email = $online_request('email');
+        $job -> requested_online = 1;
+
+        $job->save();
+
+        // Show flashing message
+        session()->flash('message', 'Your job has been submitted. Our manager will contact you 
+        shortly via provided email');
+        session()->flash('alert-class', 'alert-success');
+
+        // Redirect to home directory
+        return redirect()->home();
     }
 }
