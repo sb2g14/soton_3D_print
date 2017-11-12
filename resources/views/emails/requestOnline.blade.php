c38033df361424e29d4c152462963a8dbe787ce8
whimsial <ai1v14@soton.ac.uk>
2017-11-12 15:10:36 +0000

    Finished first email workflow
    Finished first email workflow

--- resources/views/emails/requestOnline.blade.php
+++ resources/views/emails/requestOnline.blade.php
@@ -1,30 +1,30 @@
 @component('mail::message')
 
-# Dear {{ $name }}
+    # Dear {{ $name }}
 
-<h3>You have a new online request from {{$customer_name}}</h3>
+    <h3>You have a new online request from {{$customer_name}}</h3>
 
-<p>The details of a requested job are as follows:</p>
-<div class="alert alert-info text-left">
-    <p>
-        Requested on: <b>{{ $created_at->toDayDateTimeString() }}</b><br>
-        Requested by: <b>{{$customer_name}}</b><br>
-        Requester id: <b>{{$customer_id}}</b><br>
-        Requester email: <b>{{$customer_email}}</b><br>
-        Module name or cost code: <b>{{$use_case}}</b><br>
-    </p>
-</div>
-
-@component('mail::button', ['url' => 'https://soton3dprint.clients.soton.ac.uk'])
-Go to the website
-@endcomponent
+    <p>The details of a requested job are as follows:</p>
+    <div class="alert alert-info text-left">
+        <p>
+            Requested on: <b>{{ $created_at->toDayDateTimeString() }}</b><br>
+            Requested by: <b>{{$customer_name}}</b><br>
+            Requester id: <b>{{$customer_id}}</b><br>
+            Requester email: <b>{{$customer_email}}</b><br>
+            Module name or cost code: <b>{{$use_case}}</b><br>
+        </p>
+    </div>
 
 
-@component('mail::vendor.mail.html.promotion.button', ['url' => 'https://dropoff.soton.ac.uk/pickup.php?claimID='.$claim_id.'&claimPasscode='.
-$claim_passcode.'&emailAddr='.explode("@",$email)[0].'%40soton.ac.uk'])
-Download .stl files
-@endcomponent
+    @component('mail::button', ['url' => 'https://dropoff.soton.ac.uk/pickup.php?claimID='.$claim_id.'&claimPasscode='.
+        $claim_passcode.'&emailAddr='.explode("@",$email)[0].'%40soton.ac.uk'])
+        Download .stl files
+    @endcomponent
 
-Thanks<br>
-{{ config('app.name') }}
+    @component('mail::button', ['url' => 'https://soton3dprint.clients.soton.ac.uk'])
+        Go to the website
+    @endcomponent
+
+    Kind Regards,<br>
+    {{ config('app.name') }}
 @endcomponent
