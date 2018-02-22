a289ce3dadfa15b1e88860266949b00456b86f82
GHLasse <lassew@hotmail.de>
2018-02-22 15:44:49 +0000

    working on #114 - added remaining validation functions from other files.

--- resources/assets/js/validations.js
+++ resources/assets/js/validations.js
@@ -12,7 +12,7 @@ module.exports = {
         $(target).removeClass("parsley-error");
         $(target).addClass("parsley-success");
     },
-
+//PERSON RELATED CHECKS
     check_name: function (fieldname) {
         /*checks name fields if they are correct and returns a boolean. 
          *Also sets the Error on the specified field. The error div needs to have 
@@ -20,7 +20,7 @@ module.exports = {
         var localerror = true;
         var name = $(fieldname);
 
-        if (name.val().length < 3 || name.val().length > 100) {
+        if (name.val().length < 2 || name.val().length > 20) {
             module.exports.addError(fieldname, "The name should be between 2 and 20 characters");
             localerror = true;
         } else if (!name.val().match(/^[a-z ,.'-]+$/i)) {
@@ -33,7 +33,7 @@ module.exports = {
         //check_all_fields();
         return localerror;
     },
-    check_email: function (fieldname) {
+    check_university_email: function (fieldname) {
         /*checks university email fields if they are correct and returns
          *a boolean.
          *Also sets the Error on the specified field. The error div needs to have
@@ -105,7 +105,27 @@ module.exports = {
         //check_all_fields();
         return localerror;
     },
-    check_printer_number: function (fieldname) {
+    check_password: function (fieldname) {
+        /*checks password fields if they are correct and returns
+         *a boolean.
+         *Also sets the Error on the specified field. The error div needs to have
+         *the identical fieldname but with _error appended.*/
+        var localerror = true;
+        var password = $(fieldname);
+        if(password.val().length < 6 || password.val().length > 16){
+            module.exports.addError(fieldname, "The password must be 6 to 16 character long and contain at least one upper case letter, one lower case letter, and one digit");
+            localerror = true;
+        } else if(!password.val().match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,16}$/)){
+            module.exports.addError(fieldname, "The password must be 6 to 16 character long and contain at least one upper case letter, one lower case letter, and one digit");
+            localerror = true;
+        } else {
+            module.exports.removeError(fieldname);
+            localerror = false;
+        }
+        return localerror;
+    },
+//PRINTER RELATED CHECKS
+    check_printer_number_select: function (fieldname) {
         /*checks that a printer number has been selected from the drop-down list*/
         var localerror = true;
         var printerno = $(fieldname).find(":selected").text();
@@ -119,7 +139,97 @@ module.exports = {
         }
         return localerror;
     },
+    check_printer_number_input: function (fieldname) {
+        /*checks printer number fields if they are correct and returns
+         *a boolean.
+         *Also sets the Error on the specified field. The error div needs to have
+         *the identical fieldname but with _error appended.*/
+        var localerror = true;
+        var printerno = $(fieldname).val();
 
+        if(printerno.length < 1 || printerno.length > 3){
+            module.exports.addError(fieldname, "Printer number should contain between 1 and 3 digits");
+            localerror = true;
+        }else if(!printerno.match(/^[0-9]+$/)){
+            module.exports.addError(fieldname, "Only digits are allowed");
+            localerror = true;
+        }else {
+            module.exports.removeError(fieldname);
+            localerror = false;
+        }
+        return localerror;
+    },
+    check_printer_serial: function (fieldname) {
+        /*checks printer serial number fields if they are correct and returns
+         *a boolean.
+         *Also sets the Error on the specified field. The error div needs to have
+         *the identical fieldname but with _error appended.*/
+        var localerror = true;
+        var printerno = $(fieldname).val();
+        if(printerno.length < 1 || printerno.length > 20){
+            module.exports.addError(fieldname, "The serial number must be between 1 and 20 digits long");
+            localerror = true;
+        }else if(!printerno.match(/^[A-Z a-z-0-9]+$/)){
+            module.exports.addError(fieldname, "Only letters, numbers and hyphens (-) are allowed");
+            localerror = true;
+        }else {
+            module.exports.removeError(fieldname);
+            localerror = false;
+        }
+        return localerror;
+    },
+    check_printer_type_radio: function (fieldname,otherfieldname) {
+        /*checks printer type radio button group if they are correct and returns
+         *a boolean. Also checks if the "Other" option was selected and shows the 
+         *otherfieldname which should have an _group appended to it. (i.e. if 
+         *otherfieldname is "#other", then #other_group will be shown or hidden 
+         *depending on the users selection.
+         *Also sets the Error on the specified field. The error div needs to have
+         *the identical fieldname but with _error appended.*/
+        var localerror = true;
+        var fieldvalue = $('input[name = "'.fieldname.'"]:checked').val();
+
+        if (!fieldvalue){
+            module.exports.addError(fieldname, "Please select a printer type");
+            localerror = true;
+        } else {
+            module.exports.removeError(fieldname);
+            localerror = false;
+            if(fieldvalue !== "Other"){
+                //should be like "#printer_type_other_group" to hide field and label
+                $(otherfieldname.concat("_group")).hide();
+            } else {
+                $(otherfieldname.concat("_group")).show();
+                module.exports.check_printer_type_input(otherfieldname,fieldname);
+            }
+        }
+        return localerror;
+    },
+    check_printer_type_input: function (fieldname,radiogroup) {
+        /*checks printer serial number fields if they are correct and returns
+         *a boolean.
+         *Also sets the Error on the specified field. The error div needs to have
+         *the identical fieldname but with _error appended.*/
+        var localerror = true;
+        var fieldvalue = $(fieldname).val();
+        var radioselection = $('input[name = "'.radiogroup.'"]:checked').val();
+        if(radioselection !== "Other" ){
+            module.exports.removeError(fieldname);
+            localerror = false;
+        }else if(fieldvalue.length < 1 || fieldvalue.length > 20){
+            //TODO: check if this should be 10, 20 or 100 -> I found different number sin different files, so check with database!
+            module.exports.addError(fieldname, "The printer type must be between 1 and 20 characters long");
+            localerror = true;
+        }else if(!fieldvalue.match(/^[a-z A-Z0-9.,!]+$/)){
+            module.exports.addError(fieldname, "Not permitted characters are inputted");
+            localerror = true;
+        }else {
+            module.exports.removeError(fieldname);
+            localerror = false;
+        }
+        return localerror;
+    },
+//PRINT RELATED CHECKS
     check_print_duration: function (hrsdropdown,mindropdown,group) {
         /*checks print duration fields if they are correct and returns
          *a boolean. Requires a reference to the drop-down for hours and
@@ -258,7 +368,6 @@ module.exports = {
         //check_all_fields();
         return localerror;
     },
-
     check_job_title: function (fieldname) {
         /*check Job Title fields if they are correct and returns a boolean
          *also sets the Error on the specified field. The error div needs to have
@@ -276,7 +385,25 @@ module.exports = {
             module.exports.removeError(fieldname);
             localerror = false;
         }
-        //check_all_fields();
+        return localerror;
+    },
+    check_issue_title: function (fieldname) {
+        /*check Issue Title fields if they are correct and returns a boolean
+         *also sets the Error on the specified field. The error div needs to have
+         *the identical fieldname but with _error appended.*/
+        var localerror = true;
+        var issue_title = $(fieldname).val();
+
+        if(issue_title.length < 8 || issue_title.length > 180) {
+            module.exports.addError(fieldname, 'The title should be between 8 and 180 characters');
+            localerror = true;
+        } else if(!issue_title.match(/^[a-zA-Z0-9 .,!?']+$/)){
+            module.exports.addError(fieldname,"Only these special characters are allowed: ,.!?'");
+            localerror = true;
+        } else {
+            module.exports.removeError(fieldname);
+            localerror = false;
+        }
         return localerror;
     },
     check_message: function (fieldname,minlength,maxlength) {
