14eeedf3657f889f589b4bdc4853e57d5f2f11c2
svetun <sb2g14@soton.ac.uk>
2018-04-15 16:43:43 +0100

    Former members excluder from the mailing list for annoumvements, full list added to rota email

--- resources/views/emails/announcementNew.blade.php
+++ resources/views/emails/announcementNew.blade.php
@@ -1,16 +1,11 @@
 @component('mail::message')
-
-# Dear {{ $name }}
+# Dear Demonstrator,<br/>
 
 {{ $message }}
 
 @component('mail::button', ['url' => 'https://3dprint.clients.soton.ac.uk'])
 3D printing workshop
 @endcomponent
-
-{{--@component('mail::panel', ['url' => ''])--}}
-{{--3D printing is awesome!--}}
-{{--@endcomponent--}}
 
 Thanks,<br>
 {{  $from }},
